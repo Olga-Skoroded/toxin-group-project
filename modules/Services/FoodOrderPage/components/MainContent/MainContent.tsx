5804fdc34a29aead931b5355bfd92290c1a3ffe4
Yuri Osyka <yuri.osyka@fullstack-development.com>
2020-12-04 09:49:04 +0300

    added redirects to login page

--- modules/Services/FoodOrderPage/components/MainContent/MainContent.tsx
+++ modules/Services/FoodOrderPage/components/MainContent/MainContent.tsx
@@ -1,12 +1,44 @@
-import { memo } from 'react';
+import { useRouter } from 'next/router';
+import { memo, useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { preloadAuthData } from 'redux/Auth/redux/actions';
+import { AppState } from 'redux/store.model';
 
 import { FoodOrderForm } from '../FoodOrderForm/FoodOrderForm';
 import * as S from './MainContent.styles';
 
-const MainContent = memo(() => (
-  <S.MainContent>
-    <FoodOrderForm />
-  </S.MainContent>
-));
+type StateProps = {
+  isAuthSuccess: boolean;
+};
 
-export { MainContent };
+const mapState = (state: AppState): StateProps => ({
+  isAuthSuccess: state.auth.isAuthSuccess,
+});
+
+const mapDispatch = {
+  checkAuthBeforePageLoaded: preloadAuthData,
+};
+
+type Props = StateProps & typeof mapDispatch;
+
+const MainContent = memo(({ isAuthSuccess, checkAuthBeforePageLoaded }: Props) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    checkAuthBeforePageLoaded();
+
+    if (typeof isAuthSuccess === 'boolean') {
+      if (!isAuthSuccess) router.push('/auth/login');
+    }
+  });
+
+  return (
+    <S.MainContent>
+      <FoodOrderForm />
+    </S.MainContent>
+  );
+});
+
+const ConnectedComponent = connect(mapState, mapDispatch)(MainContent);
+export { ConnectedComponent as MainContent };
